0b3b50218040fc238d8b226049212d79bf3c0d9e
nuvaaf2 <nuwaaf2@gmail.com>
2026-06-04 21:34:12 +0530

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.py
@@ -0,0 +1,43 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end_of_the_word = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+        
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+        for char in word:
+            if char not in curr.children:
+                curr.children[char] = TrieNode()
+
+            curr = curr.children[char]
+        curr.is_end_of_the_word = True
+        
+    def search(self, word: str) -> bool:
+        def dfs(j, root):
+            curr = root
+            for i in range(j, len(word)):
+                char = word[i]
+
+                if char == ".":
+                    for child in curr.children.values():
+
+                        if dfs(i + 1, child):
+                            return True
+                    return False
+                else:
+                    if char not in curr.children:
+                        return False
+                    curr = curr.children[char]
+
+            return curr.is_end_of_the_word
+
+        return dfs(0, self.root)
+
+
+        
